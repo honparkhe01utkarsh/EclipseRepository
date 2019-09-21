07446e48f9e8db2f0ee7bb84bb338c4f0b08036a
Utkarsh Honparkhe <honparkhe01utkarsh@gmail.com>
2019-09-21 20:01:38 +0530

    My first commit to move the eclipse project to github remote repository from local repo

---
+++ myGitEclipseProject/src/myGitEclipsePackage/GITEclipse.java
@@ -0,0 +1,12 @@
+package myGitEclipsePackage;
+
+public class GITEclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is khan and i am not a terrorist");
+		System.out.println("I love my india");
+		System.out.println("Ye mera india");
+	}
+
+}
